810774b6ce5ab403414b8cbdde661f018d42d158
Hamzatanweer786 <htanweer.ee41ceme@ee.ceme.edu.pk>
2023-07-15 21:02:01 +0500

    loadingdone

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
+import { FaThumbsUp } from 'react-icons/fa';
 
 export default function Page() {
   return (
     <div>
       <div className="py-10 flex flex-col justify-center items-center w-full mt-20 gap-5">
-        <p className="text-brown font-bold text-9xl leading-[200px] w-52 h-52 text-center rounded-full bg-orange-200">
-          ✓
-        </p>
-        <h1>Success</h1>
+        <h1 className='text-brown text-6xl font-bold'>Ordered Successfully</h1>
         <p className="text-center text-2xl font-roboto">
           We received your purchase request,
           <br /> we&apos;ll be in touch shortly!
